c5ca9786241b3a7c1e9b338a0bd40c8d8c4916f6
AudCavalier <valiantgab@gmail.com>
2019-02-09 20:35:47 -0600

    Added logic to determine if a line exists or not

--- line_detect/src/line_detect.cpp
+++ line_detect/src/line_detect.cpp
@@ -14,9 +14,12 @@ static const std::string OPENCV_WINDOW = "Image window";
 static const std::string GRAY = "Gray";
 static const std::string BLUR = "HSV";
 static const std::string CANNY = "Canny";
+
 using namespace cv;
 using namespace std;
 using namespace Eigen;
+
+int line_exists_flag;
 
 void extract_black(Mat* src, Mat* dst, double th_g, int skip_step_);
 void calc_variance(Mat* src, vector<int>* variance, int th, int direction, int skip_step_);
@@ -32,9 +35,9 @@ class ImageConverter{
 public:
   ImageConverter()
     : it_(nh_){
-      image_sub_ = it_.subscribe("/bebop/image_raw", 1, &ImageConverter::imageCb, this);
+      //image_sub_ = it_.subscribe("/bebop/image_raw", 1, &ImageConverter::imageCb, this);
       //ESTO LO HAGO PARA TRABAJAR SIN TENER QUE TENER EL BEBOP PRENDIDO TODO EL TIEMPO
-      //image_sub_ = it_.subscribe("/camera/image", 1, &ImageConverter::imageCb, this);
+      image_sub_ = it_.subscribe("/camera/image", 1, &ImageConverter::imageCb, this);
       image_pub_ = it_.advertise("/image_converter/output_video", 1);
 
       cv::namedWindow(OPENCV_WINDOW);
@@ -99,68 +102,84 @@ public:
       //GaussianBlur(valueMask, blurr, Size(3, 3), 0, 0);
       Canny(blurr, edge, 120, 180, 3);
       HoughLinesP(edge, lines, 1, CV_PI/180, 1, 3, 16);
-      std::vector<Vec4i> pointsVec;
-      int currx1=lines[0][0], currx2;
-      int ymin1=1000, ymin2=1000;
+      //std::cout << "LINES SIZE after: "<<lines.size() << std::endl;
+      
+      vector<int> pointsVec;
+      //std::cout << "POINTS SIZE init: "<< pointsVec.size() << std::endl;
+      /// ::::::::::::::::::::::::: VERY IMPORTANT ::::::::::::::::::::::::::
+      /*
+      El mínimo de segmentos de linea que necesitamos para decir que hay o no una linea es 4
+      Por lo que si HoughLinesP no detecta 4 o más sub-segmentos de recta, esto implica que no se detectó
+      ninguna linea (no existe o no se detectó correctamente, trabajamos con condiciones de laboratorio por lo que
+      debería ser el primer caso nada más, cualquiera que continue este trabajo y salga de estas condiciones deberá
+      considerar el segundo caso), en este caso, no podemos construir ni una linea significativa para el drone
+      y "nos salimos" de esta función, al menos hasta que termine el programa o se detecte tal linea
+      */
+      if(lines.size()<4){
+        std::cout << "Not Enough Line Segments Detected\n";
+        return;
+      }
+      /*
+      La siguiente lógica nos permitirá detectar los puntos extremos de los segmentos de linea detectados
+      por HouhgLinesP; para ubicar una linea que sea significativa para el drone sólo necesitamos "aislar"
+      dichos puntos extremos, y obtener el punto de enmedio en X, y extender la linea sobre las Ys
+      */
+      int currx1=lines[0][0], currx2; //Para los valores de x de la linea significativa
+      //Variables temporales para saber si el valor en y actual es el mínimo en todos los segmentos detectados
+      int ymin1=1000, ymin2=1000; 
+      //Variables temporales para saber si el valor en y actual es el máximo en todos los segmentos detectados
       int ymax1=0, ymax2=0;
       int tmp1[2], tmp2[2], tmp3[2], tmp4[2]; //puntos temporales extremos
-      if(lines.size()<3){
-        tmp1[0] = 0;
-        tmp1[1] = 0;
-        tmp2[0] = 0;
-        tmp2[1] = 0;
-        tmp3[0] = 0;
-        tmp3[1] = 0;
-        tmp4[0] = 0;
-        tmp4[1] = 0;
-      }else{
-        for(size_t i=1; i<lines.size(); i++){
-          if(lines[i][0]>currx1+20 || lines[i][0]<=currx1-20){
-            currx2=lines[i][0];
+      //Buscamos el punto extremo mayor en X para la X2
+      for(size_t i=1; i<lines.size(); i++){
+        if(lines[i][0]>currx1+20 || lines[i][0]<=currx1-20){
+          currx2=lines[i][0];
+        }
+      }
+      
+      //std::cout << "X VALUES GOT:\nX1: " << currx1 << " X2: " << currx2 << "\n";
+      //Conseguimos los puntos con extremos superior e inferior (en Y)
+      for (size_t i=0; i<lines.size(); i++){
+        if(lines[i][0]<=currx1+20 && lines[i][0]>currx1-20){
+          if(lines[i][1]>ymax1){
+            tmp1[0] = lines[i][0];
+            tmp1[1] = lines[i][1];
+            ymax1 = lines[i][1];
+          }
+          if(lines[i][3]<ymin1){
+            tmp2[0] = lines[i][2];
+            tmp2[1] = lines[i][3];
+            ymin1=lines[i][3];
           }
         }
-        //std::cout << "X VALUES GOT:\nX1: " << currx1 << " X2: " << currx2 << "\n";
-        //Conseguimos los puntos con extremos superior e inferior
-        for (size_t i=0; i<lines.size(); i++){
-          //std::cout << lines[i][0] << " == " << currx2 << "\n";
-          if(lines[i][0]<=currx1+20 && lines[i][0]>currx1-20){
-            if(lines[i][1]>ymax1){
-              tmp1[0] = lines[i][0];
-              tmp1[1] = lines[i][1];
-              ymax1 = lines[i][1];
-            }
-            if(lines[i][3]<ymin1){
-              tmp2[0] = lines[i][2];
-              tmp2[1] = lines[i][3];
-              ymin1=lines[i][3];
-            }
+        else if(lines[i][0]<=currx2+20 && lines[i][0]>currx2-20){
+          if(lines[i][1]>ymax2){
+            tmp3[0] = lines[i][0];
+            tmp3[1] = lines[i][1];
+            ymax2 = lines[i][1];
           }
-          else if(lines[i][0]<=currx2+20 && lines[i][0]>currx2-20){
-            if(lines[i][1]>ymax2){
-              tmp3[0] = lines[i][0];
-              tmp3[1] = lines[i][1];
-              ymax2 = lines[i][1];
-            }
-            if(lines[i][3]<ymin2){
-              tmp4[0] = lines[i][2];
-              tmp4[1] = lines[i][3];
-              ymin2=lines[i][3];
-            }
+          if(lines[i][3]<ymin2){
+            tmp4[0] = lines[i][2];
+            tmp4[1] = lines[i][3];
+            ymin2=lines[i][3];
           }
         }
       }
+      //Líneas para debug
       /*std::cout << "POINTS GOT\n";
       std::cout << "X1: " << tmp1[0] << " Y1: " << tmp1[1] << "\n";
       std::cout << "X2: " << tmp2[0] << " Y2: " << tmp2[1] << "\n";
       std::cout << "X3: " << tmp3[0] << " Y3: " << tmp3[1] << "\n";
       std::cout << "X4: " << tmp4[0] << " Y4: " << tmp4[1] << "\n";
       std::cout << "MID: " << (tmp1[0]+tmp3[0])/2 << "\n";*/
-      int linep1[2], linep2[2]; //LA LINEA ACTUAL PARA EL DRONE
-      linep1[0] = (tmp1[0]+tmp3[0])/2;
-      linep1[1] = tmp1[1];
-      linep2[0] = (tmp2[0]+tmp4[0])/2;
-      linep2[1] = tmp4[1];
+      //Agregamos al vector de puntos el promedio en ambos extremos de X y los extremos de Y
+      //Consiguiendo una linea en el centro
+      pointsVec.push_back((tmp1[0]+tmp3[0])/2);
+      pointsVec.push_back(tmp1[1]);
+      pointsVec.push_back((tmp2[0]+tmp4[0])/2);
+      pointsVec.push_back(tmp4[1]);
       //std::cout << lines.size() << "\n";
+      //std::cout << "POINTSVEC after: "<<pointsVec.size() << std::endl;
       for(size_t i=0; i<lines.size(); i++){
         Vec4i l = lines[i];
         line(cv_ptr->image(Rect(120, 400, 536, 80)), Point(l[0], l[1]), Point(l[2], l[3]), Scalar(0, 255, 0), 2, 0);
@@ -168,7 +187,8 @@ public:
         std::cout << "LINE 0 " << l[0] << "  LINE 1 " << l[1] << std::endl;
         std::cout << "LINE 2 " << l[2] << "  LINE 3 " << l[3] << std::endl;*/
       }
-      line(cv_ptr->image(Rect(120, 400, 536, 80)), Point(linep1[0], linep1[1]), Point(linep2[0], linep2[1]), Scalar(0, 0, 255), 2, 0);
+      //Dibujamos la linea significativa para el drone de color rojo
+      line(cv_ptr->image(Rect(120, 400, 536, 80)), Point(pointsVec[0], pointsVec[1]), Point(pointsVec[2], pointsVec[3]), Scalar(0, 0, 255), 2, 0);
       
       //cv::imshow(OPENCV_WINDOW, cv_ptr_copy->image);
       cv::imshow(OPENCV_WINDOW, cv_ptr->image);
@@ -184,6 +204,7 @@ public:
 };
 
 int main(int argc, char** argv){
+  line_exists_flag=0;
   ros::init(argc, argv, "image_converter");
   ImageConverter ic;
   ros::spin();
