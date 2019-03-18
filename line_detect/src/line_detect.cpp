0ff2effa3c8d26aa8160ea1d148d2085b124a7f4
AudCavalier <valiantgab@gmail.com>
2019-03-18 13:45:51 -0600

    Refactor; based on other existing codes, final ver_1

--- line_detect/src/line_detect.cpp
+++ line_detect/src/line_detect.cpp
@@ -2,211 +2,152 @@
 #include <image_transport/image_transport.h>
 #include <cv_bridge/cv_bridge.h>
 #include <sensor_msgs/image_encodings.h>
-
+#include "std_msgs/String.h"
+#include <geometry_msgs/Twist.h>
+#include "geometry_msgs/Pose.h"
+#include "geometry_msgs/PoseArray.h"
+#include "geometry_msgs/PoseStamped.h"
 #include <opencv2/imgproc/imgproc.hpp>
 #include <opencv2/highgui/highgui.hpp>
 
-#include<Eigen/Core>
-#include<Eigen/Geometry>
+#include <Eigen/Core>
+#include <Eigen/Geometry>
 #include <iostream>
+#include <stdlib.h>
 
 static const std::string OPENCV_WINDOW = "Image window";
-static const std::string GRAY = "Gray";
-static const std::string BLUR = "HSV";
-static const std::string CANNY = "Canny";
+static const std::string OPENCV_BIN = "Binary Image";
 
 using namespace cv;
 using namespace std;
 using namespace Eigen;
 
-int line_exists_flag;
+geometry_msgs::Twist msg;
+cv_bridge::CvImagePtr cv_ptr;
 
+ros::Publisher line_pos;
+
+RNG rng(12345);
+int line_exists_flag;
+int countdown_end = 5;
+bool end_flag = false;
 void extract_black(Mat* src, Mat* dst, double th_g, int skip_step_);
 void calc_variance(Mat* src, vector<int>* variance, int th, int direction, int skip_step_);
 void get_filtered(Mat* src, Mat* filtered);
 Vector2d transform_point_homography(Vector2d point);
 
-class ImageConverter{
-  ros::NodeHandle nh_;
-  image_transport::ImageTransport it_;
-  image_transport::Subscriber image_sub_;
-  image_transport::Publisher image_pub_;
+class ImageProcess{
 
 public:
-  ImageConverter()
-    : it_(nh_){
-      //image_sub_ = it_.subscribe("/bebop/image_raw", 1, &ImageConverter::imageCb, this);
-      //ESTO LO HAGO PARA TRABAJAR SIN TENER QUE TENER EL BEBOP PRENDIDO TODO EL TIEMPO
-      image_sub_ = it_.subscribe("/camera/image", 1, &ImageConverter::imageCb, this);
-      image_pub_ = it_.advertise("/image_converter/output_video", 1);
-
-      cv::namedWindow(OPENCV_WINDOW);
-      cv::namedWindow(GRAY);
-      cv::namedWindow(BLUR);
-      cv::namedWindow(CANNY);
-      //cv::namedWindow(PLACEHOLDER);
+    ImageProcess(){
+      ros::NodeHandle n;
+      line_pos=n.advertise<geometry_msgs::Point>("Line_position", 1000);
+    }
+    ~ImageProcess(){
+      cv::destroyAllWindows();
     }
 
-    ~ImageConverter(){
-      cv::destroyWindow(OPENCV_WINDOW);
-      cv::destroyWindow(GRAY);
-      cv::destroyWindow(BLUR);
-      cv::destroyWindow(CANNY);
+    void detect_line(cv::Mat& img){
+      Mat processed_img;
+      vector<Mat> hsvChannels;
+      vector<vector<Point> > contours;
+      Mat hsvImg, gray, blurr, edge, tresh, sobel, grad_x, grad_y, abs_grad_x, abs_grad_y, valueMask;
+      vector<Vec4i> lines, hierarchy;
+      Point2f center;
+      geometry_msgs::Point mid_pos;
+      float radius;
+      float x1=img.cols/2, y1=img.rows+1, x2=img.cols/2, y2=-1.0;
+
+      //Recortamos la imagen para conseguir la región de interes
+      img = img(Rect(308, 400, 200, 80));
+      //HSV
+      cvtColor(img, hsvImg, CV_BGR2HSV);
+      split(hsvImg, hsvChannels);
+      //Para un rango de negro* (Este valor lo calculé para la imagen que tengo, puede no servir, pero se puede ajustar)
+      inRange(hsvImg, Scalar(0, 0, 0), Scalar(230, 220, 210), valueMask);
+      //bitwise_img
+      bitwise_and(img, img, processed_img, valueMask);
+      //Convirtiendo la imagen a escala de grises
+      cvtColor(processed_img, gray, CV_BGR2GRAY);
+      //Aplicando filtro gaussiano
+      GaussianBlur(gray, blurr, Size(3, 3), 0, 0);
+      //GaussianBlur(valueMask, blurr, Size(3, 3), 0, 0);
+      //Detectando bordes con Canny
+      Canny(blurr, edge, 120, 180, 3);
+      //CONTOURS
+      findContours(edge, contours, hierarchy, CV_RETR_TREE, CV_CHAIN_APPROX_SIMPLE, Point(0, 0));
+
+      //HOUGH
+      HoughLinesP(edge, lines, 1, CV_PI/180, 1, 3, 16);
+      //value mask
+      //Mat valueChannel = hsvChannels[2];
+      //Mat hueMask, saturationMask, valueMask;
+      
+      //DRAW CONTOURS:
+      Mat draw = Mat::zeros(edge.size(), CV_8UC3);
+      for(int i=0; i<contours.size(); i++){
+        minEnclosingCircle(contours[i], center, radius);
+        if(center.y<y1){
+          y1=center.y;
+          x1=center.x;
+        }
+        if(center.y>y2){
+          y2=center.y;
+          x2=center.x;
+        }
+        Scalar color = Scalar(rng.uniform(0, 255), rng.uniform(0, 255), rng.uniform(0, 255));
+        drawContours(draw, contours, i, color, 2, 8, hierarchy, 0, Point());
+      }
+      mid_pos.x = (center.x+x2)/2;
+      mid_pos.y = (center.y+y2)/2;
+      /*std::cout<<"DETECTED CENTER:";
+      std::cout<<center<<std::endl;
+      std::cout<<"CALCULATED ONE:";
+      std::cout<<Point(x1, y1)<<std::endl;
+      std::cout<<"CALCULATED TWO:";
+      std::cout<<Point(x2, y2)<<std::endl;*/
+      circle(processed_img, Point((center.x+x2)/2, (center.y+y2)/2), 5, (255, 255, 200), 2);
+      //circle(img, Point(x2, y2), 5, (0, 0, 255), 1);
+      line_pos.publish(mid_pos);
+      //std::cout << processed_img.cols << std::endl;
+      cv::imshow(OPENCV_WINDOW, draw);
+      cv::imshow(OPENCV_BIN, processed_img);
     }
 
-    void imageCb(const sensor_msgs::ImageConstPtr& msg){
-      cv_bridge::CvImagePtr cv_ptr;
-      cv_bridge::CvImagePtr cv_ptr_copy;
+
+    void imageCallback(const sensor_msgs::ImageConstPtr& msg){
       try{
         cv_ptr = cv_bridge::toCvCopy(msg, sensor_msgs::image_encodings::BGR8);
-        cv_ptr_copy = cv_bridge::toCvCopy(msg, sensor_msgs::image_encodings::BGR8);
+        cv::Mat img_copy = cv_ptr->image.clone();
+        detect_line(img_copy);
+        int get_input = cv::waitKey(30);
+        if(get_input==27){
+          end_flag = true;
+        }
       }catch(cv_bridge::Exception& e){
         ROS_ERROR("cv_bridge exception: %s", e.what());
         return;
       }
-      //PRUEBA HSV
-      Mat hsvImg;
-      vector<Mat> hsvChannels;
-      cvtColor(cv_ptr->image, hsvImg, CV_BGR2HSV);
-
-      split(hsvImg, hsvChannels);
-      //HOUGH
-      
-      //value mask
-      Mat valueChannel = hsvChannels[2];
-      
-      Mat hueMask, saturationMask, valueMask;
-
-      inRange(hsvImg, Scalar(0, 0, 0), Scalar(179, 50, 100), valueMask);
-
-      if(cv_ptr->image.empty())
-      {
-         cout << "ERROR" << endl;
-      }
-      //tamaño imagen: height = 480; width = 856
-      //Recortamos la imagen a lo largo por conveniencia y facilidad de procesamiento
-      //PARA EFECTOS DE PRUEBAS PERSONALES, RECORTO LA IMAGEN EN x TAMBIÉN
-      //SUPONEMOS CONDICIONES DE LABORATORIO, ES DECIR: PISO BLANCO, LINEA NEGRA
-      //POR LO QUE NO DEBERÍA SER NECESARIO RECORTAR LA IMAGEN EN X
-      cv_ptr_copy->image = cv_ptr_copy->image(Rect(120, 400, 536, 80));
-      //valueMask = valueMask(Rect(0, 400, 856, 40));
-
-      //EN PRINCIPIO, ES NECESARIO APLICAR FILTROS PARA DETECTAR UNA LINEA NEGRA
-      //Se extrae la parte negra de la imagen
-      Mat gray, blurr, edge, tresh, sobel, grad_x, grad_y, abs_grad_x, abs_grad_y;
-      vector<Vec4i> lines;
-
-      cvtColor(cv_ptr_copy->image, gray, CV_BGR2GRAY);
-
-      GaussianBlur(gray, blurr, Size(3, 3), 0, 0);
-      //GaussianBlur(valueMask, blurr, Size(3, 3), 0, 0);
-      Canny(blurr, edge, 120, 180, 3);
-      HoughLinesP(edge, lines, 1, CV_PI/180, 1, 3, 16);
-      //std::cout << "LINES SIZE after: "<<lines.size() << std::endl;
-      
-      vector<int> pointsVec;
-      //std::cout << "POINTS SIZE init: "<< pointsVec.size() << std::endl;
-      /// ::::::::::::::::::::::::: VERY IMPORTANT ::::::::::::::::::::::::::
-      /*
-      El mínimo de segmentos de linea que necesitamos para decir que hay o no una linea es 4
-      Por lo que si HoughLinesP no detecta 4 o más sub-segmentos de recta, esto implica que no se detectó
-      ninguna linea (no existe o no se detectó correctamente, trabajamos con condiciones de laboratorio por lo que
-      debería ser el primer caso nada más, cualquiera que continue este trabajo y salga de estas condiciones deberá
-      considerar el segundo caso), en este caso, no podemos construir ni una linea significativa para el drone
-      y "nos salimos" de esta función, al menos hasta que termine el programa o se detecte tal linea
-      */
-      if(lines.size()<4){
-        std::cout << "Not Enough Line Segments Detected\n";
-        return;
-      }
-      /*
-      La siguiente lógica nos permitirá detectar los puntos extremos de los segmentos de linea detectados
-      por HouhgLinesP; para ubicar una linea que sea significativa para el drone sólo necesitamos "aislar"
-      dichos puntos extremos, y obtener el punto de enmedio en X, y extender la linea sobre las Ys
-      */
-      int currx1=lines[0][0], currx2; //Para los valores de x de la linea significativa
-      //Variables temporales para saber si el valor en y actual es el mínimo en todos los segmentos detectados
-      int ymin1=1000, ymin2=1000; 
-      //Variables temporales para saber si el valor en y actual es el máximo en todos los segmentos detectados
-      int ymax1=0, ymax2=0;
-      int tmp1[2], tmp2[2], tmp3[2], tmp4[2]; //puntos temporales extremos
-      //Buscamos el punto extremo mayor en X para la X2
-      for(size_t i=1; i<lines.size(); i++){
-        if(lines[i][0]>currx1+20 || lines[i][0]<=currx1-20){
-          currx2=lines[i][0];
-        }
-      }
-      
-      //std::cout << "X VALUES GOT:\nX1: " << currx1 << " X2: " << currx2 << "\n";
-      //Conseguimos los puntos con extremos superior e inferior (en Y)
-      for (size_t i=0; i<lines.size(); i++){
-        if(lines[i][0]<=currx1+20 && lines[i][0]>currx1-20){
-          if(lines[i][1]>ymax1){
-            tmp1[0] = lines[i][0];
-            tmp1[1] = lines[i][1];
-            ymax1 = lines[i][1];
-          }
-          if(lines[i][3]<ymin1){
-            tmp2[0] = lines[i][2];
-            tmp2[1] = lines[i][3];
-            ymin1=lines[i][3];
-          }
-        }
-        else if(lines[i][0]<=currx2+20 && lines[i][0]>currx2-20){
-          if(lines[i][1]>ymax2){
-            tmp3[0] = lines[i][0];
-            tmp3[1] = lines[i][1];
-            ymax2 = lines[i][1];
-          }
-          if(lines[i][3]<ymin2){
-            tmp4[0] = lines[i][2];
-            tmp4[1] = lines[i][3];
-            ymin2=lines[i][3];
-          }
-        }
-      }
-      //Líneas para debug
-      /*std::cout << "POINTS GOT\n";
-      std::cout << "X1: " << tmp1[0] << " Y1: " << tmp1[1] << "\n";
-      std::cout << "X2: " << tmp2[0] << " Y2: " << tmp2[1] << "\n";
-      std::cout << "X3: " << tmp3[0] << " Y3: " << tmp3[1] << "\n";
-      std::cout << "X4: " << tmp4[0] << " Y4: " << tmp4[1] << "\n";
-      std::cout << "MID: " << (tmp1[0]+tmp3[0])/2 << "\n";*/
-      //Agregamos al vector de puntos el promedio en ambos extremos de X y los extremos de Y
-      //Consiguiendo una linea en el centro
-      pointsVec.push_back((tmp1[0]+tmp3[0])/2);
-      pointsVec.push_back(tmp1[1]);
-      pointsVec.push_back((tmp2[0]+tmp4[0])/2);
-      pointsVec.push_back(tmp4[1]);
-      //std::cout << lines.size() << "\n";
-      //std::cout << "POINTSVEC after: "<<pointsVec.size() << std::endl;
-      for(size_t i=0; i<lines.size(); i++){
-        Vec4i l = lines[i];
-        line(cv_ptr->image(Rect(120, 400, 536, 80)), Point(l[0], l[1]), Point(l[2], l[3]), Scalar(0, 255, 0), 2, 0);
-        /*std::cout << "FOR i: " << i << "\n";
-        std::cout << "LINE 0 " << l[0] << "  LINE 1 " << l[1] << std::endl;
-        std::cout << "LINE 2 " << l[2] << "  LINE 3 " << l[3] << std::endl;*/
-      }
-      //Dibujamos la linea significativa para el drone de color rojo
-      line(cv_ptr->image(Rect(120, 400, 536, 80)), Point(pointsVec[0], pointsVec[1]), Point(pointsVec[2], pointsVec[3]), Scalar(0, 0, 255), 2, 0);
-      
-      //cv::imshow(OPENCV_WINDOW, cv_ptr_copy->image);
-      cv::imshow(OPENCV_WINDOW, cv_ptr->image);
-      
-      //Debugs visuales
-      cv::imshow(BLUR, valueMask);
-      cv::imshow(GRAY, gray);
-      cv::imshow(CANNY, edge);
-      cv::waitKey(3);
-
-      image_pub_.publish(cv_ptr_copy->toImageMsg());
     }
 };
 
+
 int main(int argc, char** argv){
-  line_exists_flag=0;
   ros::init(argc, argv, "image_converter");
-  ImageConverter ic;
-  ros::spin();
+  ros::NodeHandle nh_;
+  image_transport::ImageTransport it_(nh_);
+  image_transport::Subscriber image_sub_;
+  ImageProcess ip;
+  image_sub_ = it_.subscribe("/bebop/image_raw", 1, &ImageProcess::imageCallback, &ip);
+  //ESTO LO HAGO PARA TRABAJAR SIN TENER QUE TENER EL BEBOP PRENDIDO TODO EL TIEMPO
+  //image_sub_ = it_.subscribe("/camera/image", 1, &ImageProcess::imageCallback, &ip);
+  
+  while(nh_.ok()){
+    ros::spinOnce();
+    if(end_flag){
+      break;
+    }
+  }
+  cv::destroyAllWindows();
   return 0;
 }
---
+++ transfer_velocity/src/transfer_velocity.cpp
@@ -0,0 +1,143 @@
+#include <ros/ros.h>
+#include <image_transport/image_transport.h>
+#include <cv_bridge/cv_bridge.h>
+#include <sensor_msgs/image_encodings.h>
+#include <std_msgs/Empty.h>
+#include "std_msgs/String.h"
+#include <geometry_msgs/Twist.h>
+#include "geometry_msgs/Pose.h"
+#include "geometry_msgs/PoseArray.h"
+#include "geometry_msgs/PoseStamped.h"
+#include <opencv2/imgproc/imgproc.hpp>
+#include <opencv2/highgui/highgui.hpp>
+
+#include <Eigen/Core>
+#include <Eigen/Geometry>
+#include <iostream>
+#include <stdlib.h>
+
+using namespace cv;
+using namespace std;
+using namespace Eigen;
+
+static const std::string FAKE_WINDOW = "Fake window";
+
+std_msgs::Empty takeoff_msg, land_msg;
+geometry_msgs::Twist vel_bebop;
+
+ros::Publisher takeoff_pub; //tópico para despegar
+ros::Publisher land_pub; //tópico para aterrizar
+ros::Publisher vel_pub; //tópico para mover el bebop
+ros::Subscriber sub; //para subscribirse al tópico del punto 
+
+bool end_flag = false;
+int can_move = 0; //variable para indicar que el bebop empezará a moverse(1) o no (0)
+class VelocityHandle{  
+public:
+  VelocityHandle(){
+      ros::NodeHandle n;
+      sub = n.subscribe("Line_position", 1000, &VelocityHandle::chatterCallback, this);
+      vel_bebop.linear.x = 0;
+      vel_bebop.linear.y = 0;
+      vel_bebop.linear.z = 0;
+    }
+
+    ~VelocityHandle(){
+      //De-constructor
+    }
+
+    void velHandle(const geometry_msgs::Point &msg){
+      //Si Point.y >0 MOVER, si no DETENER
+      //Si Point.x >width/2+20 || Point.x < width/2-20 -> girar, si no, no girar
+      //std::cout << "OK\n";
+      int y_point = msg.y;
+      int x_point = msg.x;
+      float pitch = 0.0, yaw = 0.0;
+      std::cout << msg << std::endl;
+      if(can_move){
+        if (y_point>0){
+          //std::cout << "MOVVING FORWARD\n";
+          pitch = 0.01;
+        }else{
+          pitch = 0.0;
+        }
+        vel_bebop.linear.x = pitch;
+        if (x_point<85 || x_point>115){
+          //std::cout << "ROTATING\n";
+          //detenemos primero el bebop para que gire
+          vel_bebop.linear.x = 0;
+          vel_bebop.linear.y = 0;
+          vel_bebop.linear.z = 0;
+          vel_bebop.angular.z = 0.01;
+        }else{
+          std::cout << "NOT ROTATING\n";
+          vel_bebop.angular.z = 0;
+        }
+        //std::cout << "Pitch: " << pitch << " Yaw: " << yaw << "\n";
+        //vel_pub.publish(vel_bebop);
+      }
+    }
+
+    void chatterCallback(const geometry_msgs::Point &msg){
+      velHandle(msg);
+    }
+
+    void fakeimgCallback(const sensor_msgs::ImageConstPtr &msg){
+      //Este método es simplemente para poder acceder al teclado con cv::waitKey
+      try{
+        cv::Mat1f Fake_img(1, 1, 0.0f);
+        cv::imshow(FAKE_WINDOW, Fake_img);
+        int get_input = cv::waitKey(30);
+        if(get_input==27){
+          end_flag = true;
+        }
+        if(get_input == 't'){
+          //std::cout << "KEY: " << get_input << " TAKE OFF\n";
+          ROS_INFO("DESPEGANDO...");
+          //DESCOMENTAR PARA USAR EL NODO :: TENER PRECAUCIÓN
+          //takeoff_pub.publish(takeoff_msg);
+        }
+        if(get_input == 'l'){
+          //std::cout << "KEY: " << get_input << " LAND\n";
+          ROS_INFO("ATERRIZANDO...");
+          //land_pub.publish(land_msg);
+        }
+        if(get_input == 's'){
+          //std::cout << "KEY: " << get_input << " LAND\n";
+          can_move = 1 - can_move;
+          if(can_move){
+            ROS_INFO("INICIANDO...");
+          }else{
+            ROS_INFO("TERMINANDO...");
+          }
+        }
+      }catch(cv_bridge::Exception& e){
+        ROS_ERROR("cv_bridge exception: %s", e.what());
+        return;
+      }
+    }
+};
+
+int main(int argc, char** argv){
+  ros::init(argc, argv, "velocity_handle");
+  ros::NodeHandle nh_;
+  image_transport::ImageTransport it_(nh_);
+  image_transport::Subscriber fake_image_sub_;
+  VelocityHandle vh;
+  //fake_image_sub_ = it_.subscribe("/camera/image", 1, &VelocityHandle::fakeimgCallback, &vh);
+  fake_image_sub_ = it_.subscribe("/bebop/image_raw", 1, &VelocityHandle::fakeimgCallback, &vh);
+  //ESTO LO HAGO PARA TRABAJAR SIN TENER QUE TENER EL BEBOP PRENDIDO TODO EL TIEMPO
+  std::cout << "Menú:\nPresionar 't' para despegar\nPresionar 'l' para aterrizar\n";
+  std::cout << "Presionar 's' para iniciar/terminar (el drone debe estár en el aire)\nPresionar 'esc' para salir\n";
+  //takeoff_pub = nh_.advertise<std_msgs::Empty>("/bebop/takeoff", 1000);
+  //land_pub = nh_.advertise<std_msgs::Empty>("/bebop/land", 1000);
+  //vel_pub = nh_.advertise<geometry_msgs::Twist>("/bebop/cmd_vel", 1000);
+  while(nh_.ok()){
+    ros::spinOnce();
+    if(end_flag){
+      break;
+    }
+  }
+  cv::destroyWindow(FAKE_WINDOW);
+  return 0;
+}
